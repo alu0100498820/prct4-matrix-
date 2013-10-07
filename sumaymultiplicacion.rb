2c8cb40943ba9e0a76c10009e7c29453ecc078f8
Borja Barrera Villagrasa <alu0100498820@ull.edu.es>
2013-10-07 20:57:25 +0100

    mezcla de la rama

--- sumaymultiplicacion.rb
+++ sumaymultiplicacion.rb
@@ -1,12 +1,36 @@
 #SUMA y MULTIPLICACION DE MATRICES
 
 #!/usr/bin/ruby
+a = [[2,4,2],[1,6,0],[4,2,3]]  #matriz a
+b = [[0,2,3],[5,3,0],[2,2,2]]  #matriz b
+c = Array.new		        #matriz c
 
-a = [[1, 2, 3], [1, 0, 0], [1, 2, 2]]	# matriz a
-b = [[0, 0, 5], [7, 5, 0], [2, 1, 1]]   # matriz b
+m,n = a.size, a[1].size
 
-c = Array.new
+for i in 0...m do
+  c[i]=Array.new
+    for j in 0...n do
+      c[i][j] = 0
+    end
+end
 
-m,n = a.size,a[1].size
+for i in 0...m do
+  for j in 0...n do
+    c[i][j]= a[i][j] + b[i][j]
+  end
+end
 
+puts "\n La Matriz a es ="
+a.each do |fila|
+  puts fila.join(" ")
+end
 
+puts "\n La Matriz b es ="
+b.each do |fila|
+  puts fila.join(" ")
+end
+
+puts "\n La Suma de la Matriz a y b es ="
+c.each do |fila|
+  puts fila.join(" ")
+end

b9cda823617c918b247ae8e3aa8ed7e4d7ba5736
Borja Barrera Villagrasa <alu0100498820@ull.edu.es>
2013-10-07 19:39:15 +0100

    Inicializando matrices

---
+++ sumaymultiplicacion.rb
@@ -0,0 +1,12 @@
+#SUMA y MULTIPLICACION DE MATRICES
+
+#!/usr/bin/ruby
+
+a = [[1, 2, 3], [1, 0, 0], [1, 2, 2]]	# matriz a
+b = [[0, 0, 5], [7, 5, 0], [2, 1, 1]]   # matriz b
+
+c = Array.new
+
+m,n = a.size,a[1].size
+
+

9660445e9c012988685c26541624fdd5b13b827e
Borja Barrera Villagrasa <alu0100498820@ull.edu.es>
2013-10-07 20:37:10 +0100

    suma de matrices

--- sumaymultiplicacion.rb
+++ sumaymultiplicacion.rb
@@ -1,4 +1,4 @@
-#SUMA y MULTIPLICACION DE MATRICES
+#SUMA Y MULTIPLICACION DE MATRICES
 
 #!/usr/bin/ruby
 
@@ -7,6 +7,38 @@ b = [[0, 0, 5], [7, 5, 0], [2, 1, 1]]   # matriz b
 
 c = Array.new
 
-m,n = a.size,a[1].size
+m,n = a.size, a[1].size
+
+for i in 0...m do
+  c[i] = Array.new
+  for j in 0...n do
+    c[i][j] = 0
+  end
+end
+
+for i in 0...m do
+  for j in 0...n do
+    c[i][j] = a[i][j] + b[i][j]
+  end
+end
+
+puts "\nLa matriz a es: "
+
+a.each do |fila|
+  puts fila.join(" ")
+end
+
+puts "\nLa matriz b es: "
+
+b.each do |fila|
+  puts fila.join(" ")
+end
+
+puts "\nLa matriz resultante es: "
+
+c.each do |fila|
+  puts fila.join(" ")
+end
+
 
 
